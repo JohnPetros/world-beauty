d683002e3f9e27754137ce700d53576fe23a6c23
JohnPetros <joaopedro.nc@outlook.com>
2024-09-01 19:15:07 -0300

    🥸 fakers: customers

---
+++ src/__tests__/fakers/index.ts
@@ -0,0 +1,4 @@
+export { CustomersFaker } from './customers-faker'
+export { PhonesFaker } from './phones-faker'
+export { ProductsFaker } from './products-faker'
+export { ServicesFaker } from './services-faker'
