916912697a453defe5e403dc315f4336a02934ee
JohnPetros <joaopedro.nc@outlook.com>
2024-09-02 10:45:24 -0300

    ✨ use cases: delete service

---
+++ src/core/use-cases/services/delete-service.ts
@@ -0,0 +1,42 @@
+import type { Service } from '@/core/entities'
+import type { Input, Output } from '../../interfaces'
+import { Delete } from '../delete'
+import { ListServices } from '../listing'
+
+export class DeleteService extends Delete {
+  private isRunning = true
+  private services: Service[]
+
+  constructor(services: Service[], input: Input, output: Output) {
+    super(input, output)
+    this.services = services
+  }
+
+  public async delete(): Promise<void> {
+    const servicesList = new ListServices(this.services, this.input, this.output)
+    servicesList.list()
+
+    while (this.isRunning) {
+      const id = await this.input.text('ID do produto:')
+
+      const service = this.services.find((service) => service.id === id)
+
+      if (!service) {
+        this.output.error('Produto não encontrado')
+        continue
+      }
+
+      await this.deleteService(service)
+      this.isRunning = false
+    }
+  }
+
+  public async deleteService(service: Service): Promise<void> {
+    const serviceIndex = this.services.findIndex((currentService) =>
+      currentService.isEqualTo(service),
+    )
+    this.services.splice(serviceIndex, 1)
+
+    this.output.success('Produto deletado com sucesso')
+  }
+}
