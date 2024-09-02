ad99343890bf643908378ef874c897d91022ad08
JohnPetros <joaopedro.nc@outlook.com>
2024-09-02 10:43:59 -0300

    ✨ use cases: list services

---
+++ src/core/use-cases/listing/list-services.ts
@@ -0,0 +1,27 @@
+import type { Service } from '../../entities'
+import type { Input, Output } from '../../interfaces'
+import { List } from './list'
+
+export class ListServices extends List {
+  private services: Service[]
+
+  constructor(services: Service[], input: Input, output: Output) {
+    super(input, output)
+    this.services = services
+  }
+
+  public list(): void {
+    if (!this.services.length) {
+      this.output.error('Nenhum serviço encontrado')
+      return
+    }
+
+    this.output.table(
+      this.services.map((service) => ({
+        Nome: service.name,
+        Preco: service.price,
+        Descrição: service.description,
+      })),
+    )
+  }
+}
