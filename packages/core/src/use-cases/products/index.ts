107825156b341e9f19ade7707d284abd39fb6732
JohnPetros <joaopedro.nc@outlook.com>
2024-09-17 12:20:43 -0300

    ✨ use cases: list pproducts

---
+++ packages/core/src/use-cases/products/index.ts
@@ -0,0 +1,2 @@
+export { ListProductsUseCase } from './list-products-use-case'
+export { RegisterProductUseCase } from './register-product-use-case'
---
+++ packages/core/src/use-cases/products/list-products-use-case.ts
@@ -0,0 +1,15 @@
+import type { IProductsRepository } from '../../interfaces'
+import { PaginationResponse } from '../../responses/pagination'
+
+export class ListProductsUseCase {
+  constructor(private readonly productsRepository: IProductsRepository) {}
+
+  async execute(page: number) {
+    const [products, productsCount] = await Promise.all([
+      this.productsRepository.findAllPaginated(page),
+      this.productsRepository.count(),
+    ])
+
+    return new PaginationResponse({ items: products, itemsCount: productsCount })
+  }
+}
